50fa7c2c1aa65df8e91eb83849fe45323354578d
Sharif Marmol <marmol.sharif@gmail.com>
2022-08-17 19:48:20 -0300

    fetch

--- main.js
+++ main.js
@@ -1,3 +1,54 @@
+
+window.addEventListener('load', ()=>{
+
+    let lon;
+    let lat;
+
+    let temperaturaValor = document.getElementById('temperaturaValor');
+    let temperaturaDescripcion = document.getElementById('temperaturaDescripcion');
+    
+    let ubicacion = document.getElementById('temperaturaValor');
+    let icono = document.getElementById('icono');
+
+    
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(posicion => {
+            lon = posicion.coords.longitude;
+            lat = posicion.coords.latitude;
+
+            const url = 'https://api.openweathermap.org/data/3.0/weather?lat=${lat}&lon=${lon}&appid={545248949c060fcc462871118becc18a}';
+
+            fetch(url)
+                .then(response => {return response.json()} )
+                .then(data => {
+
+                    let temp = Math.round(data.main.temp);
+                    temperaturaValor.textContent = '${temp}°C';
+
+                    let desc = data.weather[0].description;
+                    temperaturaDescripcion.textContent = desc.toUpperCase();
+
+                    ubicacion.textContent = data.name;
+
+                    let iconCode = (data.weather[0].icon);
+                    const urlIcon = 'http://openweathermap.org/img/wn/${iconCode}.png'
+
+                })
+                .catch(error =>{
+                    console.log(error);
+                })
+        })
+    }
+})
+
+
+
+
+
+
+
+
+
 const contenedorTareas = document.querySelector("#contenedorTareas");
 
 const añadirTareas = document.querySelector("#añadirTareas");
