a7d8cd6659171b458272a5afeb9f046dee974a38
Sharif Marmol <marmol.sharif@gmail.com>
2022-09-04 21:48:17 -0300

    ultima entrega

--- main.js
+++ main.js
@@ -1,54 +1,4 @@
 
-window.addEventListener('load', ()=>{
-
-    let lon;
-    let lat;
-
-    let temperaturaValor = document.getElementById('temperaturaValor');
-    let temperaturaDescripcion = document.getElementById('temperaturaDescripcion');
-    
-    let ubicacion = document.getElementById('temperaturaValor');
-    let icono = document.getElementById('icono');
-
-    
-    if(navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(posicion => {
-            lon = posicion.coords.longitude;
-            lat = posicion.coords.latitude;
-
-            const url = 'https://api.openweathermap.org/data/3.0/weather?lat=${lat}&lon=${lon}&appid={545248949c060fcc462871118becc18a}';
-
-            fetch(url)
-                .then(response => {return response.json()} )
-                .then(data => {
-
-                    let temp = Math.round(data.main.temp);
-                    temperaturaValor.textContent = '${temp}°C';
-
-                    let desc = data.weather[0].description;
-                    temperaturaDescripcion.textContent = desc.toUpperCase();
-
-                    ubicacion.textContent = data.name;
-
-                    let iconCode = (data.weather[0].icon);
-                    const urlIcon = 'http://openweathermap.org/img/wn/${iconCode}.png'
-
-                })
-                .catch(error =>{
-                    console.log(error);
-                })
-        })
-    }
-})
-
-
-
-
-
-
-
-
-
 const contenedorTareas = document.querySelector("#contenedorTareas");
 
 const añadirTareas = document.querySelector("#añadirTareas");
@@ -75,30 +25,13 @@ añadirTareas.addEventListener("click", (e)=>{
         tarea.addEventListener("click", cambiarEstado);
         tarea.textContent = textoTarea.value;
         contenedorTareas.prepend(tarea);
-        guardarStorage();
         textoTarea.value = "";
     }
 });
 
-let listadoTareas = [];
-
-const guardarStorage = (tarea) => {
-
-    if(tarea !== null){
-        listadoTareas.push(textoTarea.value);
-        let echoOporhacer = contenedorTareas.childNodes.forEach( el => {
-            el.classList.contains("echo") ?  estado = "Echo": estado = "Por Hacer";
-        })
-        
-        localStorage.setItem(listadoTareas,estado);
-        
-    }
-};
-
 
 const cambiarEstado = event => {
     event.target.classList.toggle("echo");
-    guardarStorage();
 
 };
 
